6a08764929f3af40045b26bf4807718c7e470980
josephmb830 <josephmoran@duck.com>
2024-04-22 20:37:59 -0500

    Cálculo del total de compras diarias

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-let totalApagar = 0; // Inicialización de la variable que acumula el total a pagar
+const comprar = () => {
 
-for (let index = 1; index <= 3; index++) {
-  const nombre = prompt("¿Qué producto desea llevar?"); // Solicitar el nombre del producto
-  let cantidad = Number(prompt("¿Cuántas unidades?")); // Solicitar la cantidad y convertir a número
-  let precio = Number(prompt("¿Cuánto sale cada unidad?")); // Solicitar el precio y convertir a número
+  let totalApagar = 0; // Inicialización de la variable que acumula el total a pagar
 
-  const subtotal = cantidad * precio; // Calcular el subtotal
-  totalApagar += subtotal; // Sumar el subtotal al total a pagar
-  alert(cantidad + " unidades de " + nombre + " = " + subtotal);
-  console.log(cantidad + " unidades de " + nombre + " = " + subtotal);
+  for (let index = 1; index <= 3; index++) {
+    const nombre = prompt("¿Qué producto desea llevar?"); // Solicitar el nombre del producto
+    let cantidad = Number(prompt("¿Cuántas unidades?")); // Solicitar la cantidad y convertir a número
+    let precio = Number(prompt("¿Cuánto sale cada unidad?")); // Solicitar el precio y convertir a número
+
+    const subtotal = cantidad * precio; // Calcular el subtotal
+    totalApagar += subtotal; // Sumar el subtotal al total a pagar
+    alert(cantidad + " unidades de " + nombre + " = " + subtotal);
+    console.log(cantidad + " unidades de " + nombre + " = " + subtotal);
+  }
+
+  const mensajeTotal = "El resultado del total a pagar es " + totalApagar;
+
+  alert(mensajeTotal); // Mostrar el total a pagar
+  console.log(mensajeTotal); // Mostrar el total a pagar
+  return totalApagar;
+
 }
 
-const mensajeTotal = "El resultado del total a pagar es " + totalApagar;
 
-alert(mensajeTotal); // Mostrar el total a pagar
-console.log(mensajeTotal); // Mostrar el total a pagar
+const total1 = comprar();
+const total2 = comprar();
+const total3 = comprar();
+const total = total1 + total2 + total3;
+
+const mensajeGlobal = "El resultado del total a de la suma de los totales de los clientes es " + total;
+
+alert(mensajeGlobal); // Mostrar el total a pagar
+console.log(mensajeGlobal); // Mostrar el total a pagar
