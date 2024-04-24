aad9d186927afcf6fe7b571a4ac25e5f155ace8d
josephmb830 <josephmoran@duck.com>
2024-04-23 20:15:51 -0500

    creación de un producto

---
+++ scripts/products.js
@@ -0,0 +1,14 @@
+const producto1 = {
+    nombre: "celular",
+    precio: 100000,
+    stock: 2
+    };
+
+    producto1.id = "id123";
+    producto1["foto"] = "https://i.postimg.cc/Jn2C5W84/galaxy1.webp";
+
+console.log(producto1);
+alert(producto1);
+
+console.log(producto1.nombre);
+alert(producto1.nombre);
