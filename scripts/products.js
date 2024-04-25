b205e074c37a3014692d9ae64590fef6a80edcb4
josephmb830 <josephmoran@duck.com>
2024-04-24 20:53:17 -0500

    Obtención y modificación de propiedades de una instancia

--- scripts/products.js
+++ scripts/products.js
@@ -14,19 +14,37 @@ console.log(producto1.nombre);
 alert(producto1.nombre);
 
 class Product {
-  constructor(id, title, price, stock, images, onsale) {
+  constructor(id, title, price, stock, images, onsale, supplier) {
     this.id = id;
     this.title = title;
     this.price = price;
     this.stock = stock;
     this.images = images;
     this.onsale = onsale;
+    this._supplier = supplier;
+  }
+
+  get getSupplier() {
+    return this._supplier;
+  }
+
+  set setSupplier(newName) {
+    this._supplier = newName;
   }
 }
 
 const prod1 = new Product();
 const prod2 = new Product(455, "manzana", 30);
 const prod3 = new Product(40, "pera", 55, 3, "image", 20);
+const prod4 = new Product(
+  80,
+  "papitas",
+  2.5,
+  20,
+  "papita_image",
+  15,
+  "frilays"
+);
 
 console.log(prod1);
 alert(prod1);
@@ -37,8 +55,20 @@ alert(prod2);
 console.log(prod3);
 alert(prod3);
 
+console.log(prod4);
+alert(prod4);
+
 console.log(prod2.title);
 alert(prod2.title);
 
 console.log(prod3.onsale);
 alert(prod3.onsale);
+
+console.log(prod4._supplier);
+alert(prod4._supplier);
+
+prod4._supplier = "alicorp";
+
+// Imprimir el valor de los getters en la consola
+console.log("Supplier:", prod4._supplier); // Imprime: Supplier: alicorp
+alert("Supplier:", prod4._supplier); // Imprime: Supplier: alicorp
